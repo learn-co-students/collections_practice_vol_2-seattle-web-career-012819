7dad415c4d547d36b92c03d328c1d6ee19cbb417
Emily Wagner <emilywagner598@gmail.com>
2019-01-31 00:15:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def begins_with_r (array)
   array.each do |word|
     if word[0,1] == "r"
@@ -37,13 +39,54 @@ def remove_non_strings(array)
 end
 
 def count_elements (array)
+  new_array = []
+  new_hash = {}
 
+  array.each do |element|
+    if new_hash.keys.include?(element[:name]) == false
+      new_hash[element[:name]] = 1
+    else
+      new_hash[element[:name]] += 1
+    end
+  end
+
+  new_hash.each do |k,v|
+    temp_hash = Hash.new
+    temp_hash[:name] = k
+    temp_hash[:count] = v
+    new_array.push(temp_hash)
+  end
+  new_array
+end
+
+def merge_data(hash1, hash2)
+  hash1.each do |element|
+    hash2[0][element[:first_name]].each do |k,v|
+      element[k] = v
+    end
+  end
 end
 
 def find_cool (array)
+  new_array = []
   array.each do |element|
-    if element[:tempature].to_s == "cool"
-      return element
+    if element[:temperature] == "cool"
+      new_array << element
     end
   end
+  new_array
+end
+
+def organize_schools(array)
+  loc_hash = {}
+
+  array.each do |k|
+    if loc_hash.keys.include?(k[1][:location]) == false
+      loc_hash[k[1][:location]] = [k[0]]
+    else
+      loc_hash[k[1][:location]] << k[0]
+    end
+  #  binding.pry
+  end
+  loc_hash
 end
